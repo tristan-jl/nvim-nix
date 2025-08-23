731901a0a20ce3866be1c2f3e67ee0aefb15c521
Tristan Laurens <tristanlaurens@hotmail.com>
2025-08-23 14:14:39 +0100

    Rm deprecated

--- plugin/keymap.lua
+++ plugin/keymap.lua
@@ -6,7 +6,7 @@ set("", "<C-c", "<Esc>")
 
 -- Highlight on yank
 local yankGroup = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", { command = "silent! lua vim.highlight.on_yank()", group = yankGroup })
+vim.api.nvim_create_autocmd("TextYankPost", { command = "silent! lua vim.hl.on_yank()", group = yankGroup })
 
 --Map blankline
 vim.g.indent_blankline_char = "┊"
